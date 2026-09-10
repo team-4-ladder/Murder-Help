a6b4247ce9046aebb576eeaeb398a1cb63aa0433
momo <ptjkjm1@gmail.com>
2026-09-10 17:25:15 +0900

    feat: Order 도메인 및 관련 리포지토리 초기 구현

---
+++ src/main/java/org/example/murderhelp/domain/order/repository/OrderItemRepository.java
@@ -0,0 +1,20 @@
+package org.example.murderhelp.domain.order.repository;
+
+import org.example.murderhelp.domain.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+//  TODO: LEFT JOIN reviews r ON oi.id = r.order_item_id 추가
+    @Query("""
+    SELECT oi FROM OrderItem oi
+    WHERE oi.order.id IN :orderIds
+    ORDER BY oi.id ASC
+    """)
+    List<OrderItem> findAllWithReviewByOrderIdIn(@Param("orderIds") List<Long> orderIds);
+
+}
