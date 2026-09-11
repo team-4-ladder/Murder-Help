209a95a491009e69a3e5babf86841dda50112d52
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-09-11 10:38:20 +0900

    feat: 공통 에러코드 추가

--- src/main/java/org/example/murderhelp/global/error/ErrorCode.java
+++ src/main/java/org/example/murderhelp/global/error/ErrorCode.java
@@ -20,6 +20,8 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "회원을 찾을 수 없습니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "MEMBER_002", "이미 존재하는 이메일입니다."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "MEMBER_003", "이메일 또는 비밀번호가 올바르지 않습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "MEMBER_004", "유효하지 않은 리프레시 토큰입니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "MEMBER_005", "리프레시 토큰을 찾을 수 없습니다."),
 
     // Product
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT_001", "상품을 찾을 수 없습니다."),
@@ -53,6 +55,7 @@ public enum ErrorCode {
 
     // 멤버 에러
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "MEMBER_001", "회원을 찾지 못했습니다."),
+  
 
     // Refund
     REFUND_NOT_FOUND(HttpStatus.NOT_FOUND, "REFUND_001", "존재하지 않는 환불 건입니다."),
