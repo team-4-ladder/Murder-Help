de694ec6bb3cfd16426cd33b14fde2692e796555
YunJeongMin <wjdals2229@naver,com>
2026-09-14 10:08:13 +0900

    feat : refund 웹훅 기능 임시 주석 처리

--- src/main/java/org/example/murderhelp/domain/refund/facade/RefundFacade.java
+++ src/main/java/org/example/murderhelp/domain/refund/facade/RefundFacade.java
@@ -70,6 +70,7 @@ public class RefundFacade {
         return RefundResponse.from(savedRefund);
     }
 
+    /*
     public void syncCancelFromPg(String portonePaymentId, String reason) {
         log.info("========== [웹훅 취소 동기화 진입] ========== portonePaymentId={}", portonePaymentId);
 
@@ -82,4 +83,5 @@ public class RefundFacade {
 
         log.info("웹훅 취소 동기화 완료. Refund ID: {}", savedRefund.getId());
     }
+    */
 }
